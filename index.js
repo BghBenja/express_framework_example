d6c37923be18d577034b1ca8663b1daf11ede436
BghBenja <bghbenja@gmail.com>
2023-02-01 23:43:27 +0100

    Implement Post request and add dummy data

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const fs = require('fs');
 
 app.get('/products', (req, res) =>{
     res.send('GET /products');
@@ -9,8 +11,22 @@ app.get('/products/:productId', (req, res) => {
     res.send('GET /products/id');
 });
 
-app.post('/products', (req, res) => {
-    res.send('POST /products');
+app.post('/products', bodyParser.json(), (req, res) => {
+    
+    const newProduct = {
+        id: uuidv4(),
+        name: sanitizeString(req.body.name),
+        price: Number(req.body.price),
+        isInStock: Boolean(req.body.isInStock),
+    };
+
+    fs.readFile('./data/products.json' , (err, file) => {
+        const products = JSON.parse(file);
+        products.push(newProduct);
+        fs.writeFile('./data/products.json', JSON.stringify(products), (err) => {
+            res.send(newProduct);
+        })
+    })
 });
 
 app.put('/products/:productId', (req, res) => {
@@ -22,3 +38,15 @@ app.delete('/products/:productId', (req, res) => {
 });
 
 app.listen(3000);
+
+function sanitizeString(str){
+    str = str.replace(/[^a-z0-9áéíóúñü \.,_-]/gim,"");
+    return str.trim();
+}
+
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+}
